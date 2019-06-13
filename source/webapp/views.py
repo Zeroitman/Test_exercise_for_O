3b455aa9b26b8ad439a599584adfcfc1fbd104df
Zeroitman <borsh130894@gmail.com>
2019-06-13 16:34:00 +0600

    №4. Made the separation of hashes by number and letter

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,10 +2,34 @@ import requests
 from webapp.models import User
 from django.shortcuts import render, redirect
 from django.views.generic import DetailView, ListView
+import re
 
 
 def index_view(request):
     return render(request, 'index.html')
+
+
+class Separation():
+    def __init__(self, md5, sha1, sha2):
+        self.md5 = md5
+        self.sha1 = sha1
+        self.sha2 = sha2
+
+    def all(self, hash):
+        number_in = re.findall(r'\d+', hash)
+        str_in = re.findall(r'\D+', hash)
+        conv_number_in, conv_str_in = ''.join(number_in), ''.join(str_in)
+        conv_number_and_string = conv_number_in + conv_str_in
+        return conv_number_and_string
+
+    def separation_md5(self):
+        return self.all(self.md5)
+
+    def separation_sha1(self):
+        return self.all(self.sha1)
+
+    def separation_sha2(self):
+        return self.all(self.sha2)
 
 
 def get_user(request):
@@ -18,8 +42,17 @@ def get_user(request):
     sha1 = user_data_for_mail['results'][0]['login']['sha1']
     sha256 = user_data_for_mail['results'][0]['login']['sha256']
     email_constructor = first_name[0:2] + last_name + '@' + city + '.' + country_code.lower()
-    user = User.objects.create(all_user_data=user_data_for_mail, email=email_constructor, md5=md5, sha1=sha1,
-                               sha256=sha256, first_name=first_name.capitalize(), last_name=last_name.capitalize())
+    a = Separation(md5=md5, sha1=sha1, sha2=sha256)
+    md5_ready = a.separation_md5()
+    sha1_ready = a.separation_sha1()
+    sha256_ready = a.separation_sha2()
+    user = User.objects.create(all_user_data=user_data_for_mail,
+                               email=email_constructor.replace(' ', ''),
+                               md5=md5_ready,
+                               sha1=sha1_ready,
+                               sha256=sha256_ready,
+                               first_name=first_name.capitalize(),
+                               last_name=last_name.capitalize())
     return redirect('user_page', pk=user.pk)
 
 
