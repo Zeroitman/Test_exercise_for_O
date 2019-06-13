d84b08ca9e116c8f759e678c94fef019ab159c0a
Zeroitman <borsh130894@gmail.com>
2019-06-14 00:35:00 +0600

    №6. Add link in base template and in api, upgrade user page

--- source/api_v1/serializers.py
+++ source/api_v1/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:user-detail')
+
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'md5', 'sha1', 'sha256', 'all_user_data')
+        fields = ('url', 'first_name', 'last_name', 'email', 'md5', 'sha1', 'sha256', 'all_user_data')
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=255, verbose_name='Имя пользователя', null=True, blank=True)
-    last_name = models.CharField(max_length=255, verbose_name='Фамилие пользователя', null=True, blank=True)
-    email = models.EmailField(verbose_name='Почтовый ящик', null=True, blank=True)
-    md5 = models.TextField(verbose_name='Разделённый md5', null=True, blank=True)
-    sha1 = models.TextField(verbose_name='Разделённый sha1', null=True, blank=True)
-    sha256 = models.TextField(verbose_name='Разделённый sha256', null=True, blank=True)
+    first_name = models.CharField(max_length=255, verbose_name='Имя пользователя')
+    last_name = models.CharField(max_length=255, verbose_name='Фамилие пользователя')
+    email = models.EmailField(verbose_name='Почтовый ящик')
+    md5 = models.TextField(verbose_name='Разделённый md5')
+    sha1 = models.TextField(verbose_name='Разделённый sha1')
+    sha256 = models.TextField(verbose_name='Разделённый sha256')
     all_user_data = models.TextField(verbose_name='Все полученные данные')
 
     def __str__(self):
